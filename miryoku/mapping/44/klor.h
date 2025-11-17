25382dcc2676a1ec23da11ef8d85636fd6233b75
Andrii Prokhorovych <prohorovychua@gmail.com>
2025-11-17 14:40:24 +0200

    fix

--- miryoku/mapping/44/klor.h
+++ miryoku/mapping/44/klor.h
@@ -22,7 +22,7 @@
 //     >;
 // };
 
-#define SQT &kp SQT
+// #define SQT &kp SQT
 // #define MUT &kp C_MUTE
 // #define PPM &kp K_PP
 
@@ -33,7 +33,7 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
      K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09 \
-XXX  K10  K11  K12  K13  K14        K15  K16  K17  K18  K19  SQT \
+XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  SQT \
 XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
                K32  K33  K34  XXX       XXX  K35  K36  K37
 
