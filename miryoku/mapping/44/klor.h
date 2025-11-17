09a9849a140e37804943923a0bb82253cfec5291
Andrii Prokhorovych <prohorovychua@gmail.com>
2025-11-17 13:38:13 +0200

    feat: add few triggers

--- miryoku/mapping/44/klor.h
+++ miryoku/mapping/44/klor.h
@@ -4,6 +4,20 @@
 #if !defined (MIRYOKU_LAYOUTMAPPING_KLOR)
 
 #define XXX &none
+#define HYP &kp LS(LC(LA(LGUI)))
+#define SQT &kp SQT
+#define MUT &kp C_MUTE
+#define PPM &kp K_PP
+
+&sensors {
+    left_encoder {
+        triggers-per-rotation = <15>;
+    };
+
+    right_encoder {
+        triggers-per-rotation = <15>;
+    };
+};
 
 #define MIRYOKU_LAYOUTMAPPING_KLOR( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
@@ -12,8 +26,8 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
      K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09 \
-XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
+HYP  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  SQT \
+XXX  K20  K21  K22  K23  K24  MUT       PPM  K25  K26  K27  K28  K29  XXX \
                K32  K33  K34  XXX       XXX  K35  K36  K37
 
 #endif
